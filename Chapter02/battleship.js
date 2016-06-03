3ae7013c30b4b0bf46a7d1f9b715ec582fd0d99b
biuleong91 <leong_wen@hotmail.com>
2016-06-03 18:07:43 +0800

    20160603

--- Chapter02/battleship.js
+++ Chapter02/battleship.js
@@ -1,6 +1,6 @@
-var location1=3;
-var location2=4;
-var location3=5;
+var location1=Math.floor(Math.random()*5);;
+var location2=location1+1;
+var location3=location2+1;
 var guess;
 var hits=0;
 var guesses=0;
@@ -28,3 +28,4 @@ while(!isSunk){
 var stats = "You took  " + guesses + " guesses to sink  the battleship, " + "which means your shooting accuracy is " + 3/guesses*100 + "%.";
 console.log(stats);
 alert(stats);
+alert("The location of the battleship was: "+ location1 + ", " + location2 + ", " + location3);
--- helloworld.js
+++ helloworld.js
@@ -2,3 +2,5 @@
 var message = "Howdy" + " " + "JavaScript";
 console.log(message);
 
+var rand = Math.floor(Math.random()*5);
+console.log(rand);
