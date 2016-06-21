f1aacba7ccf52db73ffe327ca882ce267cce49fc
biuleong91 <leong_wen@hotmail.com>
2016-06-21 18:04:27 +0800

    MP201606211804

---
+++ basicSyntax.js
@@ -0,0 +1,101 @@
+var message = "Howdy" + " " + "JavaScript\n";
+console.log(message);
+
+var rand = Math.floor(Math.random() * 5);
+
+console.log("The random number is: " + rand + "\n");
+
+function takeaway(a, b) {
+    return c = a - b;
+}
+
+console.log("Subtraction method: " + takeaway(1, 3) + "\n");
+
+var array01 = [0, 1, 2, 3, 4];
+token = array01[2];
+console.log("The index 2 of array01 is: " + token + "\n");
+
+array01[1] = 10;
+console.log("Assign the new value(10) to the array01 at index 1: " + array01 + "\n");
+
+numArray01 = array01.length;
+console.log("The number of items of array01 is: " + numArray01 + "\n");
+
+var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+var last = products.length - 1;
+var recent = products[last]; //products[products.length-1]
+console.log("The recent item of products is: " + recent + "\n");
+
+var array02 = [1, 2, "abc", true];
+console.log("A array of various types is : " + array02 + "\n");
+
+var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
+    			34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
+    			46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44
+];
+var costs = [0.25, 0.27, 0.25, 0.25, 0.25, 0.25,
+			0.33, 0.31, 0.25, 0.29, 0.27, 0.22,
+			0.31, 0.25, 0.25, 0.33, 0.21, 0.25,
+			0.25, 0.25, 0.28, 0.25, 0.24, 0.22,
+			0.20, 0.25, 0.30, 0.25, 0.24, 0.25,
+			0.25, 0.25, 0.27, 0.25, 0.26, 0.29];
+
+var bestSolutions = [];
+
+var highestScore = 0;
+
+for (i = 0; i <= scores.length - 1; i++) {
+    console.log("Bubble solution #" + i + ": " + scores[i]);
+    if (true === findHigherScore(scores[i], highestScore)) {
+        highestScore = scores[i];
+    }
+}
+
+getBestSolutions(scores, bestSolutions);
+var index = getMostCostEffectiveSolution(scores, costs, highestScore);
+
+console.log("Bubbles test: " + scores.length);
+console.log("Highest bubble scores: " + highestScore);
+console.log("Solutoins with highest score: " + bestSolutions);
+console.log("The most cost effective solution is: " +index+ " with the score of " + scores[index] +" and the cost of " + costs[index]);
+
+function getBestSolutions(scores, bestSolutions) {
+    for (i = 0; i <= scores.length - 1; i++) {
+        if (highestScore == scores[i]) {
+            bestSolutions.push(i);
+        }
+    }
+
+}
+
+function findHigherScore(num, highestScore) {
+    if (highestScore <= num) {
+        highestScore = num;
+        return true;
+    } else
+        return false;
+}
+
+function getHighestScore(scores) {
+    var highestScore = 0;
+
+    for (var i = 0; i <= scores.length; i++) {
+        if (scores[i] > highestScore)
+            highestScore = scores[i];
+    }
+    return highestScore;
+}
+
+function getMostCostEffectiveSolution(scores, costs, highscore) {
+    var cost = 100;
+    var index;
+
+    for (var i = 0; i <= scores.length; i++) {
+        if (scores[i] == getHighestScore(scores))
+            if (cost > costs[i]){
+                index = i;
+        		cost = costs[i];
+        		}
+    }
+    return index;
+}
